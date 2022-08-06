022df4761bb31516747a3ac6699cd351a2f1194f
Matheus Oliveira <matheus_0119@outlook.com>
2022-08-06 16:31:47 -0300

    Subtotal & Total methods

--- src/main/java/com/coursejava/course/entities/Order.java
+++ src/main/java/com/coursejava/course/entities/Order.java
@@ -106,6 +106,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem ite : items) {
+			sum += ite.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
