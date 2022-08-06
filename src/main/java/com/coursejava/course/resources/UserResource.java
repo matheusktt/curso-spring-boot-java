7bf5a008dc484ee58284bc8e6a7c0c50fa4f44b4
Matheus Oliveira <matheus_0119@outlook.com>
2022-08-06 16:53:09 -0300

    User delete

--- src/main/java/com/coursejava/course/resources/UserResource.java
+++ src/main/java/com/coursejava/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,7 @@ public class UserResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-//	Aceita o id como parametro na url
+//	@PathVariable Aceita o id como parametro na url
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
@@ -41,5 +42,13 @@ public class UserResource {
 		obj =  service.Insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+//	Requisicao sem responsta (void) 
+	public ResponseEntity<Void> delete (@PathVariable Long id) {
+		service.delete(id);
+//		resposta sem corpo
+		return ResponseEntity.noContent().build();
 	}
 }
